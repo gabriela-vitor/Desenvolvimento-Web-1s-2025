219f4c8b6808a0a7e0cc886341d5bcdf28284534
GABRIELA VITOR SIQUEIRA <gabriela.vitor@a.ucb.br>
2025-03-08 10:01:18 -0300

    first commit

---
+++ aula1/script.js
@@ -0,0 +1,21 @@
+console.log("Olá mundo")
+
+var animal = "gato"
+console.log(animal)
+console.log(typeof animal)
+
+animal = "peixe"
+console.log(animal)
+
+const valor = 10
+
+//exibe na tela o nome e idade
+var nomeCompleto = window.prompt("Nome: ")
+var idade = parseInt(window.prompt("Idade: "))
+
+console.log(nomeCompleto)
+console.log(idade)
+console.log(typeof idade)
+
+
+console.log(`Voce se chama ${nomeCompleto} e tem ${idade} anos`)
